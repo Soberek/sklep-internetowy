a930dbecd12400a6c56049e23fc7c3e0738b388c
Sober <59960384+Soberek@users.noreply.github.com>
2025-08-18 00:48:39 +0200

    feat: add apiService for fetching all and searched products

---
+++ src/services/apiService.ts
@@ -0,0 +1,27 @@
+import type { Product } from "../types/product";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const fetchAllProducts = async ({
+  path,
+}: {
+  path?: string;
+}): Promise<Product[]> => {
+  const response = await fetch(`${API_URL}/${path}`);
+  if (!response.ok) throw new Error("Failed to fetch products");
+
+  const data = await response.json();
+  return data.products;
+};
+
+export const fetchSearchedProducts: (
+  query: string
+) => Promise<Product[]> = async (query: string) => {
+  const response = await fetch(
+    `${API_URL}/products/search?q=${encodeURIComponent(query)}`
+  );
+  if (!response.ok) throw new Error("Failed to fetch products");
+
+  const data = await response.json();
+  return data.products;
+};
